0ffb156a7f2b252d281c6ae0a1db77ea7880cee5
Vlad Firoiu <vladfi2@gmail.com>
2026-02-10 13:28:04 -0500

    Modernize packaging with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,39 +1,13 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 
-from setuptools import setup
-from distutils.extension import Extension
-from distutils.core import run_setup
-
-import os
 import glob
 import sys
-import re
 
 source_files = ["enet.pyx"]
 
 _enet_files = glob.glob("enet/*.c")
 source_files.extend(_enet_files)
-
-lib_version = "1.3.18"
-package_version = lib_version
-
-
-with open('README.rst') as f:
-   long_description = re.sub(r'[^\x00-\x7F]+', ' ', f.read())
-
-
-from distutils.command.build_ext import build_ext as _build_ext
-class build_ext(_build_ext):
-    def run(self):
-
-        from Cython.Build import cythonize
-        compiler_directives = {'language_level': 2}
-        self.extensions = cythonize(self.extensions,
-              compiler_directives=compiler_directives)
-
-        _build_ext.run(self)
-
-        extra_args = sys.argv[2:]
-        run_setup(os.path.join(os.getcwd(), "setup.py"), ['build_py'] + extra_args)
 
 
 define_macros = [('HAS_POLL', None), ('HAS_FCNTL', None),
@@ -49,29 +23,17 @@ if sys.platform != 'darwin':
     define_macros.extend([('HAS_GETHOSTBYNAME_R', None),
                           ('HAS_GETHOSTBYADDR_R', None)])
 
-ext_modules = [
-    Extension(
+ext_modules = cythonize(
+    [Extension(
         "enet",
         sources=source_files,
         include_dirs=["enet/include/"],
         define_macros=define_macros,
         libraries=libraries,
-        library_dirs=["enet/"])
-]
+        library_dirs=["enet/"])],
+    compiler_directives={'language_level': 2},
+)
 
 setup(
-    name='pyenet',
-    # packages=['enet'],
-    description='A python wrapper for the ENet library',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/piqueserver/pyenet/',
-    maintainer='Andrew Resch, Piqueserver team',
-    maintainer_email='samuel@swalladge.id.au',
-
-    version=package_version,
-    cmdclass={'build_ext': build_ext},
     ext_modules=ext_modules,
-    setup_requires=['Cython>=3,<4'],
-    install_requires=['Cython>=3,<4'],
 )
